69c08f3937682f39aa449f348a0d0952c2cf0f81
heqing749 <2125500133@qq.com>
2026-06-05 19:04:52 +0800

    Update main.js调整相机位置

--- main.js
+++ main.js
@@ -23,7 +23,8 @@ function init() {
 
     // 2. 相机
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100);
-    camera.position.set(0, 1, 4); // 初始视角
+    camera.position.set(0, 1, 10); // 初始视角
+    camera.lookat(0, 0, 0);
 
     // 3. 渲染器
     renderer = new THREE.WebGLRenderer({ antialias: true });
